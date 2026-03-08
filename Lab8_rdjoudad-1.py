7c3e89cb005ba84c8c3e7efdcbb8135f30c6051d
Ryma Djoudad <zenyod85@gmail.com>
2026-03-08 12:58:52 -0400

    Added print lines

---
+++ Lab8_rdjoudad-1.py
@@ -0,0 +1,9 @@
+import circle, rectangle
+
+print("Geometry Calculator")
+print("-------------------")
+print("1. Calculate Circle Area")
+print("2. Calculate Circle Circumference")
+print("3. Calculate Rectangle Area")
+print("4. Calculate Rectangle Perimeter")
+print("5. Exit")
