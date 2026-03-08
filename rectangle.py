77f9b78588e8f83cf1b57560b97aacb494fe3cf0
Ryma Djoudad <zenyod85@gmail.com>
2026-03-08 12:35:45 -0400

    Added rectangle functions

--- rectangle.py
+++ rectangle.py
@@ -5,4 +5,10 @@ Calculate the area and circumference of rectangles
 3/8/2026
 """
 
-def area
+def area(width, height):
+    rectangle_area = width*height
+    return rectangle_area
+
+def perimeter(width, height):
+    rectangle_perimeter = 2*(width+height)
+    return rectangle_perimeter
