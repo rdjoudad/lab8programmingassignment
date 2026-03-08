e32e6f25c03bce3f8919a67d03dc3735d4262d4e
Ryma Djoudad <zenyod85@gmail.com>
2026-03-08 15:07:36 -0400

    Added while loop with choices

--- Lab8_rdjoudad-1.py
+++ Lab8_rdjoudad-1.py
@@ -1,9 +1,30 @@
 import circle, rectangle
 
-print("Geometry Calculator")
-print("-------------------")
-print("1. Calculate Circle Area")
-print("2. Calculate Circle Circumference")
-print("3. Calculate Rectangle Area")
-print("4. Calculate Rectangle Perimeter")
-print("5. Exit")
+while True:
+    print("Geometry Calculator")
+    print("-------------------")
+    print("1. Calculate Circle Area")
+    print("2. Calculate Circle Circumference")
+    print("3. Calculate Rectangle Area")
+    print("4. Calculate Rectangle Perimeter")
+    print("5. Exit")
+
+    user_choice = int(input("Enter your choice (1-5): "))
+
+    if user_choice == 1:
+        r = float(input("Enter the radius of the circle: "))
+        print(f"Your circle's area is: {circle.area(r):.2f}")
+    elif user_choice == 2:
+        r = float(input("Enter the radius of the circle: "))
+        print(f"Your circle's circumference is: {circle.circumference(r):.2f}")
+    elif user_choice == 3:
+        l = float(input("Enter the length of the rectangle: "))
+        w = float(input("Enter the width of the rectangle: "))
+        print(f"Your rectangle's area is: {rectangle.area(l, w):.2f}")
+    elif user_choice == 4:
+        l = float(input("Enter the length of the rectangle: "))
+        w = float(input("Enter the width of the rectangle: "))
+        print(f"Your rectangle's perimeter is: {rectangle.perimeter(l, w):.2f}")
+    elif user_choice == 5:
+        print("Goodbye!")
+        break
